38826578b244a0033f63c5316e1f048622ed31ec
Tksuthar <2018pgicstarun43@poornima.org>
2021-05-26 19:10:50 +0530

    intial commit

---
+++ sign_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib import admin
+from . import views
+from django.conf.urls import handler404, handler400
+
+app_name = 'sign_app'
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('signin', views.signin, name = 'signin'),
+    path('logout', views.log_out, name = 'logout'),
+    path('signup', views.signup, name = 'signup'),
+]
+
+handler404 = 'sign_app.views.error_404'
---
+++ sign_app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import SignUpForm, SignInForm
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+
+def home(request):
+    if 'user' in request.session:
+        return render(request, 'home.html')
+    return redirect('sign_app:signin')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+
+        # create sesstion variable
+        request.session['user'] = username
+        request.session['passwrod'] = password
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('sign_app:home')
+    form = SignInForm()
+    return render(request, 'signin.html', {'form':form})
+
+def signup(request):
+    form = SignUpForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            form.save()
+            # create sesstion variable
+            request.session['user'] = username
+            request.session['passwrod'] = password
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            # redirect to home page
+            return redirect('sign_app:home')
+
+        # else
+    form = SignUpForm()
+    return render(request, 'signup.html', {'form':form})
+
+def log_out(request):
+    if request.method == 'POST':
+        request.session.flush()
+    return redirect('sign_app:signin')
+
+def error_404(request, exception):
+    return render(request, '404.html')
