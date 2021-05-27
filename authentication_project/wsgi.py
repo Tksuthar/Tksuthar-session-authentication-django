08310cf862e8e9bff450c1780c9d72dc649dbe18
Tksuthar <tksuthar7463@gmail.com>
2021-05-27 10:25:42 +0530

    wsgi changeed

--- authentication_project/wsgi.py
+++ authentication_project/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
 
+import os
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authentication_project.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
